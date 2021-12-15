eea62aeec3094fbce856fa25a88cebce3228b305
Cong Hoang <cong@coderpush.com>
2021-12-15 18:40:14 +0700

    update

--- _02_TypeScript_Examples/01-Basic/README_01.md
+++ _02_TypeScript_Examples/01-Basic/README_01.md
@@ -5,4 +5,6 @@ npm i -g typescript
 npm i -g ts-node
 ```
 
+Working with node: `npm i -D @types/node`
+
 Ref: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-oop.html
---
+++ _02_TypeScript_Examples/01-Basic/first-sample.ts
@@ -0,0 +1,17 @@
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+// console.log(add(1, 2));
+
+// //! => This will cause am error!
+// console.log(add(1, "2"));
+
+//! ========================================================================
+//! ========================================================================
+//! ========================================================================
+
+function otherAdd(a: number, b: number): number {
+  //! => This will cause an error
+  return "123";
+}
