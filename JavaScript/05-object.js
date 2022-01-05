7f697576465c5ec050bb1fa8da593514b5bc6ebc
Cong Hoang <cong@coderpush.com>
2022-01-05 11:11:17 +0700

    update

--- JavaScript/05-object.js
+++ JavaScript/05-object.js
@@ -3,8 +3,14 @@ function main05(){
     obj["name"] = "Jack";
     obj["Cap Ba"] = "12A";
     obj["CapHai"] = "8A";
-
     console.log(obj);
+
+    //! Sometime you get weird object definition
+    const otherObj1 = {['Property with space']: "ok"};
+    console.log(otherObj1);
+
+    const otherObj2 = {'Property with space': "ok"};
+    console.log(otherObj2);
 }
 
 main05();
